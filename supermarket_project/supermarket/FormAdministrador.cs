44a878097a7cd66d93306b251935e81682319dd0
Daniel Triviño <dibu.13@gmail.com>
2013-06-15 18:00:44 +0200

    Ya se muestran los nombres de los clientes existentes en la lista
    tambien cuando se crea uno nuevo aparece en la lista automaticamente

--- supermarket_project/supermarket/FormAdministrador.cs
+++ supermarket_project/supermarket/FormAdministrador.cs
@@ -16,12 +16,26 @@ namespace supermarket
         public FormAdministrador()
         {
             InitializeComponent();
+
+            StreamReader fichero_users = new StreamReader("Ficheros\\usuarios.txt");
+            string linea;
+            string[] datos = null;
+
+            while ((linea = fichero_users.ReadLine()) != null)
+            {
+                datos = linea.Split(new char[] { '#' });
+                ListaClientes.Items.Add(datos[2]);
+            }
+
+            fichero_users.Close();
+
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
 
-            supermarket_class.Administrador.crear_user(Convert.ToInt32(T_Id.Text),T_Dni.Text, T_Nombre.Text, T_Direc.Text, T_Email.Text, T_Pass.Text,Convert.ToBoolean(T_Admin.Text));                
+            supermarket_class.Administrador.crear_user(Convert.ToInt32(T_Id.Text),T_Dni.Text, T_Nombre.Text, T_Direc.Text, T_Email.Text, T_Pass.Text,Convert.ToBoolean(T_Admin.Text));
+            ListaClientes.Items.Add(T_Nombre.Text);    
         }
     }
 }
